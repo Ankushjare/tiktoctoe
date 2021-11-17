658650219234dd08cbb2523a69f071e6b4b4940b
Ankushjare <jareankush@gmail.com>
2021-11-17 11:37:44 +0530

    Added: Choose Letter which one to Play

---
+++ TikTacToe/TikTacToe/ChooseLetter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TikTacToe
+{
+    class ChooseLetter
+    {
+        public void choose()
+        {
+            Console.WriteLine("P1- Choose the letter X Otherwise O ");
+            string A = Console.ReadLine();
+            if (A == "X")
+            {
+                Console.WriteLine("P1 use X");
+            }
+            else
+            {
+                Console.WriteLine("P1 use O");
+            }
+        }
+
+    }
+}
