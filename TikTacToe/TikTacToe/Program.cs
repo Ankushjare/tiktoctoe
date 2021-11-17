1ff2aec96541e3cd3c22173a2c2354fd8a10d250
Ankushjare <jareankush@gmail.com>
2021-11-17 09:51:54 +0530

    TikTocToe game

---
+++ TikTacToe/TikTacToe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TikTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1 Obj = new UC1();
+            Obj.Board();
+        }
+    }
+}
---
+++ TikTacToe/TikTacToe/UC1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TikTacToe
+{
+    public class UC1
+    {
+        char[] array = new char[10];
+        public void Board()
+        {
+            for (int i =1; i< array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
